4ff195ca6652a29caa99989496d0ed6cd0e5e34d
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-01-28 20:05:45 +0000

    Updated to mpMemoryValgrindTest.

--- Testing/mpMemoryValgrindTest.cpp
+++ Testing/mpMemoryValgrindTest.cpp
@@ -33,11 +33,11 @@ TEST_CASE( "1. Create stack allocated array.", "[Valgrind tests]") {
   // Create an array of 10 integers.
   int myArray[10];
 
-  // Q2. Above array is stack allocated. Does it leak memory at end of scope (function).
+  // Q2. Above array is stack allocated. Does it leak memory at end of scope (function)?
 
   // Q3. Keep making and re-running the unit test, increasing the number of ints, until your program crashes. How many ints?
 
-  std::cout << "Test 1. This line is just for a break point." << std::endl;
+  std::cout << "Test 1: This line marks the end of test 1." << std::endl;
 }
 
 
@@ -49,9 +49,10 @@ TEST_CASE( "2. Create heap allocated array", "[Valgrind tests]") {
   // Q4. Its heap allocated. Does it leak memory at end of scope?
 
   // Q5. Fix it by calling delete correctly.
+  //     Note: Delete operator is different for arrays and non-arrays.
 
   // Q6. Increase the number of ints, can you allocate more than in the first test (Q3)?
-  std::cout << "Test 2: This line is just to set a breakpoint on." << std::endl;
+  std::cout << "Test 2: This line marks the end of test 2." << std::endl;
 }
 
 
@@ -68,13 +69,15 @@ private:
   int myArray[10];
 };
 
-TEST_CASE( "Create abject containing a stack allocated block of memory", "[Valgrind tests]") {
+TEST_CASE( "3. Create abject containing a stack allocated block of memory", "[Valgrind tests]") {
 
   // Q7. Does this leak memory?
   for (int i = 0; i < 100; i++)
   {
     ContainerA myObject;
   }
+
+  std::cout << "Test 3: This line marks the end of test 3." << std::endl;
 }
 
 
@@ -95,7 +98,7 @@ private:
   int *mySingleInteger;
 };
 
-TEST_CASE( "Create abject containing a heap allocated block of memory", "[Valgrind tests]") {
+TEST_CASE( "4. Create abject containing a heap allocated block of memory", "[Valgrind tests]") {
 
   // Q8. Does this leak memory?
   for (int i = 0; i < 100; i++)
@@ -105,6 +108,8 @@ TEST_CASE( "Create abject containing a heap allocated block of memory", "[Valgri
 
   // Q9. Fix it by calling delete operators correctly.
   //     Note: Delete operator is different for arrays and non-arrays.
+
+  std::cout << "Test 4: This line marks the end of test 4." << std::endl;
 }
 
 
@@ -116,26 +121,50 @@ public:
 };
 
 
-TEST_CASE( "First SmartPointer example", "[Valgrind tests]") {
+TEST_CASE( "5. Vector example", "[Valgrind tests]") {
 
-  // Q10. What's going on here?
+  // Above, we see how creating arrays and creating objects that contain objects
+  // can lead to memory leaks. But what about standard containers like std::vector?
+
+  std::vector<ThingA> myVec;
+  for (int i = 0; i < 10; i++)
+  {
+    ThingA myA;
+    std::cout << "Test 5: Created myA, i=" << i << std::endl;
+    myVec.push_back(myA);
+    std::cout << "Test 5: Added to list, i=" << i << std::endl;
+  }
+
+  // Q10. How many objects were created?
+  //      Look in the log-file if unsure.
+
+  std::cout << "Test 5: This line marks the end of test 5." << std::endl;
+
+  // Q11. What would happen therefore if ThingA had pointers to other objects?
+}
+
+TEST_CASE( "6. First SmartPointer example", "[Valgrind tests]") {
+
+  // Q12. What's going on here?
   //      Is p stack or heap allocated?
   std::unique_ptr<ThingA> p1(new ThingA);
 
   // Uncomment these and test if they work:
-  // Q11. First, do they compile?
-  // Q12. What are the implications.
+  // Q13. First, do they compile?
+  // Q14. What are the implications.
   //std::unique_ptr<ThingA> p2;
   //p2 = p1;
   //std::unique_ptr<ThingA> p3(p1);
 
-  // Q13. Then try these instead.
+  // Q15. Then try these instead.
   //std::unique_ptr<ThingA> p2(nullptr);
   //p2 = std::move(p1);
   //REQUIRE(p1 == nullptr);
   //REQUIRE(p2 != nullptr);
 
-  // Q14. Conclusion?
+  // Q16. Conclusion?
+
+  std::cout << "Test 6: This line marks the end of test 6." << std::endl;
 }
 
 
@@ -144,10 +173,10 @@ public:
   ThingB()
   : p(new ThingA())
   {
-    std::cout << "Constructing ThingB" << std::endl;
+    std::cout << "Constructing ThingB, with a pointer to ThingA" << std::endl;
 
   }
-
+  ~ThingB() { std::cout << "Destructing ThingB" << std::endl; }
 private:
   std::unique_ptr<ThingA> p;
 };
@@ -160,10 +189,12 @@ std::unique_ptr<ThingB> FunctionThatDoesSomething(std::unique_ptr<ThingB>& myObj
   return localVariable;
 }
 
-TEST_CASE( "ThingB containing ThingA", "[Valgrind tests]") {
+TEST_CASE( "7. ThingB containing ThingA", "[Valgrind tests]") {
 
   // As before, create a unique_ptr to hold a ThingB
   std::unique_ptr<ThingB> p1(new ThingB);
+
+  std::cout << "Created a unique pointer p1 to an instance of ThingB" << std::endl;
 
   // Note how:
   // a) Each time we use a SmartPointer we assign it immediately.
@@ -180,4 +211,14 @@ TEST_CASE( "ThingB containing ThingA", "[Valgrind tests]") {
   //      we pass by value instead of pass by reference.
 
   // Q16. In the definition for FunctionThatDoesSomething, what happens if we specify const for the first argument?
+
+  std::cout << "Test 7: This line marks the end of test 7." << std::endl;
+}
+
+
+TEST_CASE( "8. Shared pointer example", "[Valgrind tests]") {
+
+  // Q17. Create a shared pointer to a ThingB.
+  // Q18. Create another shared pointer to the same ThingB, using assignment.
+  // Q19. Create another shared pointer to the same ThingB, using copy constructor.
 }
